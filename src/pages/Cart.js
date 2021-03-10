265c41382367f5bd644d04d28e105fe6871d1bfb
Quang Tạ <quang.tx305@gmail.com>
2021-03-10 21:56:20 +0700

    push

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,10 +5,10 @@ import { getToken } from "../Utils/Common";
 import { getUser } from "../Utils/Common";
 import Header from "../components/Header";
 import { Link } from "react-router-dom";
-import  reqlogin  from "../Asset/Img/reques-login.png";
+import reqlogin from "../Asset/Img/reques-login.png";
 import bag from "../Asset/Img/empty-cart.png"
 import { connect } from "react-redux";
-import {fetchCartFromLocalStorage} from "../action/cartAction";
+import { fetchCartFromLocalStorage } from "../action/cartAction";
 class Cart extends React.Component {
   formatter = new Intl.NumberFormat("vi-VI", {
     style: "currency",
@@ -16,11 +16,11 @@ class Cart extends React.Component {
     minimumFractionDigits: 0,
   });
   componentDidMount() {
-    if(this.props.carts === undefined) {
+    if (this.props.carts === undefined) {
       this.props.fetchCartFromLocalStorage();
     }
   }
-   sum = (items) => {
+  sum = (items) => {
     var sum = 0;
     items.forEach((item) => {
       sum += item.price * item.qty;
@@ -52,101 +52,93 @@ class Cart extends React.Component {
   };
   checkCart = (carts) => {
     var user = getUser();
-    if (user != null) {
-      if (carts.length !== 0) {
-        return (
-          <div className="row" style={{ paddingTop: 50, paddingBottom: 50 }}>
-            <div className="col-lg-9">
-              <div className="d-flex justify-content-center">
-                <h4 className="text-white">Giỏ hàng</h4>
-              </div>
-              {carts.map((value, key) => {
-                return (
-                  <ItemCart
-                    key={key}
-                    img={value.img}
-                    name={value.gameName}
-                    price={value.price}
-                    qty={value.qty}
-                    id={value.id}
-                  />
-                );
-              })}
-              <div className="row p-1">
-                <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6 col-6 text-left">
-                  <button onClick={() =>this.sum(carts)} className="text-white btn">
-                    Tiếp tục mua hàng
-                  </button>
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6 col-6 text-right">
-                  <button
-                    onClick={() =>  this.deleteall}
-                    className="text-white btn btn-outline-danger"
-                  >
-                    Xoá giỏ hàng
-                  </button>
-                </div>
-              </div>
+    if (carts.length !== 0) {
+      return (
+        <div className="row" style={{ paddingTop: 50, paddingBottom: 50 }}>
+          <div className="col-lg-9">
+            <div className="d-flex justify-content-center">
+              <h4 className="text-white">Giỏ hàng</h4>
             </div>
-            <div className="col-lg-3">
-              <div className="d-flex justify-content-center">
-                <h4 className="text-white">Tạm tính</h4>
-              </div>
-              <div className="cart-form text-center mt-1 p-2">
-                <div className="align-items-center">
-                  <table className="table-bill table text-white text-center">
-                    <thead></thead>
-                    <tbody>
-                      <tr>
-                        <th>Số lượng</th>
-                        <td>{this.totalNumber(carts)}</td>
-                      </tr>
-                      <tr>
-                        <th>Tạm tính</th>
-                        <td>{this.formatter.format(this.sum(carts))}</td>
-                      </tr>
-                      <tr>
-                        <th>Thành tiền</th>
-                        <td>{this.formatter.format(this.sum(carts))}</td>
-                      </tr>
-                    </tbody>
-                    <tfoot></tfoot>
-                  </table>
-                </div>
-              </div>
-              <div className="mt-2">
-                <Link className="" to="/thanh-toan">
-                  <button className="btn btn-danger col-lg-12 col-md-12 col-xs-12">
-                    Tiến hành đặt hàng
+            {carts.map((value, key) => {
+              return (
+                <ItemCart
+                  key={key}
+                  img={value.img}
+                  name={value.gameName}
+                  price={value.price}
+                  qty={value.qty}
+                  id={value.id}
+                />
+              );
+            })}
+            <div className="row p-1">
+              <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6 col-6 text-left">
+                <button onClick={() => this.sum(carts)} className="text-white btn">
+                  Tiếp tục mua hàng
                   </button>
-                </Link>
+              </div>
+              <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6 col-6 text-right">
+                <button
+                  onClick={() => this.deleteall}
+                  className="text-white btn btn-outline-danger"
+                >
+                  Xoá giỏ hàng
+                  </button>
               </div>
             </div>
           </div>
-        );
-      } else {
-        return (
-          <div className="row justify-content-center">
-            <div className="empty-form col-lg-6 text-center m-4 p-2">
-                <h3 className="mt-3">Oop! Giỏ hàng trống</h3>
-                <img className="ml-4 img-fluid" alt={bag} src={bag} />
-              <Link className="px-0 mx-0 col-lg-12" to="/">
-                <h4>Trở lại cửa hàng</h4>
-              </Link>
+          <div className="col-lg-3">
+            <div className="d-flex justify-content-center">
+              <h4 className="text-white">Tạm tính</h4>
             </div>
+            <div className="cart-form text-center mt-1 p-2">
+              <div className="align-items-center">
+                <table className="table-bill table text-white text-center">
+                  <thead></thead>
+                  <tbody>
+                    <tr>
+                      <th>Số lượng</th>
+                      <td>{this.totalNumber(carts)}</td>
+                    </tr>
+                    <tr>
+                      <th>Tạm tính</th>
+                      <td>{this.formatter.format(this.sum(carts))}</td>
+                    </tr>
+                    <tr>
+                      <th>Thành tiền</th>
+                      <td>{this.formatter.format(this.sum(carts))}</td>
+                    </tr>
+                  </tbody>
+                  <tfoot></tfoot>
+                </table>
+              </div>
+            </div>
+            {
+              !user ? (<button className="btn btn-danger col-lg-12 col-md-12 col-xs-12">
+                Vui lòng đăng nhập
+              </button>) : (
+                <div className="mt-2">
+                  <Link className="" to="/thanh-toan">
+                    <button className="btn btn-danger col-lg-12 col-md-12 col-xs-12">
+                      Tiến hành đặt hàng
+                                </button>
+                  </Link>
+                </div>
+              )
+            }
           </div>
-        );
-      }
+        </div>
+      );
     } else {
       return (
         <div className="row justify-content-center">
           <div className="empty-form col-lg-6 text-center m-4 p-2">
-                <h3 className="mt-3">Bạn chưa đăng nhập</h3>
-                <img className="ml-4 img-fluid" alt={reqlogin} src={reqlogin} />
-              <Link className="px-0 mx-0 col-lg-12" to="/dang-nhap">
-                <h4 >Đăng nhập</h4>
-              </Link>
-            </div>
+            <h3 className="mt-3">Oop! Giỏ hàng trống</h3>
+            <img className="ml-4 img-fluid" alt={bag} src={bag} />
+            <Link className="px-0 mx-0 col-lg-12" to="/">
+              <h4>Trở lại cửa hàng</h4>
+            </Link>
+          </div>
         </div>
       );
     }
@@ -166,7 +158,7 @@ class Cart extends React.Component {
           }}
         >
           {
-            !carts? (<div>Loading...</div>) : (
+            !carts ? (<div>Loading...</div>) : (
               <div className="container">{this.checkCart(carts)}</div>
             )
           }
@@ -176,4 +168,4 @@ class Cart extends React.Component {
     );
   }
 }
-export default connect((state) => ({carts: state.cart.items}), {fetchCartFromLocalStorage})(Cart)
+export default connect((state) => ({ carts: state.cart.items }), { fetchCartFromLocalStorage })(Cart)
